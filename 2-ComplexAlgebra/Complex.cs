bfa97076735bdfeb726ba0a1539a2892af557309
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2022-12-07 14:34:04 +0100

    Exe 2 Complete

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,49 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
-    ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
-    /// TODO: * representing a complex number as a string or the form Re +/- iIm
-    /// TODO:     - e.g. via the ToString() method
     /// TODO: * checking whether two complex numbers are equal or not
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public Complex(double real, double imaginary)
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+        public double Real { get; }
+        public double Imaginary { get; }
+        public double Modulus() => Math.Sqrt(Real * Real + Imaginary + Imaginary);
+        public double Phase() => Math.Atan2(Imaginary, Real);
+        public Complex Complement() => new Complex(Real, -Imaginary);
+        public Complex Plus(Complex other_number) => 
+            new Complex(Real - other_number.Real, Imaginary - other_number.Imaginary);
+        public Complex Minus(Complex other_number) => 
+            new Complex(Real - other_number.Real, Imaginary - other_number.Imaginary);
+
+        public override string ToString()
+        {
+            if (Imaginary == 0.0) return Real.ToString();
+            var imAbs = Math.Abs(Imaginary);
+            var imValue = imAbs == 1.0 ? "" : imAbs.ToString();
+            string sign;
+            if (Real == 0d)
+            {
+                sign = Imaginary > 0 ? "" : "-";
+                return sign + "i" + imValue;
+            }
+
+            sign = Imaginary > 0 ? "+" : "-";
+            return $"{Real} {sign} i{imValue}";
+        }
+
+        public bool Equal(Complex other_to_compare)
+        {
+            if (other_to_compare.GetType() != GetType()) return false;
+            return Real.Equals(other_to_compare) && Imaginary.Equals(other_to_compare);
+        }
     }
 }
